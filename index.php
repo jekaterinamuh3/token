b478fa221219735813f215c5b88796f821de55d0
jekaterinamuh3 <jekaterinamuh@gmail.com>
2025-02-12 14:32:19 +0100

    Yo

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" href="style.css">
+    <title>Hello world</title>
+
+    </head>
+    <body>
+        <h1>Hello World of Jekatharina</h1>
+
+
+        
+
+        <?php
+        
+        
+    
+        
+        ?>
+       
+                
+
+
+    </body>
+
+    </html>
